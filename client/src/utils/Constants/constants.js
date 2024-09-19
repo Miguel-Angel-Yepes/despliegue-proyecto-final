0e62f266d8410be5f6ad54b5a6fcaae17d302261
miguel <135150856+mrmiguelillo@users.noreply.github.com>
2024-09-18 22:51:29 -0500

    prueba final v4

--- client/src/utils/Constants/constants.js
+++ client/src/utils/Constants/constants.js
@@ -1,8 +1,8 @@
-const SERVER_IP = "localhost:3977";
+const SERVER_IP = "https://servimascotas.onrender.com";
 
 export const ENV = {
-    BASE_PATH: `http://${SERVER_IP}`,
-    BASE_API: `http://${SERVER_IP}/api/v1`,
+    BASE_PATH: `${SERVER_IP}`,
+    BASE_API: `${SERVER_IP}/api/v1`,
     API_ROUTES: {
         REGISTER: "auth/register",
         LOGIN: "auth/login",
